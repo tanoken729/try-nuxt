3e19baafa8d5d5690d7ca72c66f037670f1da419
たのけん <tanoken729@gmail.com>
2021-05-01 18:34:26 +0900

    ElemetUIをpluginsで使用する

---
+++ pages/element.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="padding: 30px">
+        <el-button @click="visible = true">Button</el-button>
+        <el-dialog :visible.sync="visible" title="確認">
+            <p>Try Element</p>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {
+            visible: false
+        }
+    },
+})
+</script>
